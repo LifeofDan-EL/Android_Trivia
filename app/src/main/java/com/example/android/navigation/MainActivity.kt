7439af8ea4c328c5701486a2c68bd6a45d92b81f
lifeofdan_el <anomfuemedaniel@gmail.com>
2020-05-07 19:32:21 +0100

    Hooked actionBar control to navHostFragment.

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,6 +19,9 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,5 +29,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        // Finding the navController from myNavHostFragment
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        //Linking the navController to the actionbar
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    //Overrides and takes control of up bar
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
